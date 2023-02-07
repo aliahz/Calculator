09c8726b5c410ce8fd170972eb134bd7491f05f2
Mohammad Kargar <57566624+dofxo@users.noreply.github.com>
2023-02-07 18:23:04 +0330

    added js variables and some basic fundamentals

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+// variables
+
+const buttons = document.querySelectorAll('.button')
+const displayTop = document.querySelector('.displayTop')
+const displayBottom = document.querySelector('.displayBottom')
+
+
+
+// TITLE : event listner baraye har dokme dar safhe
+// bad az click shodan har dokme dar safhe, be nesbate shart haei ke vojood dare function calculate() run mishe
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        calulate(button)
+    })
+})
+
+
+
+
+// functions
+
+
+// TITLE : function calulate
+// be nesbate meqdare dokme click shode dar safhe, amaliat haye mashin hesabo namayesh mide
+
+function calculate(button) {
+
+}
