6708aeab403ed31e6568854a7464320282bf5b8b
nsrsh25 <108688245+nsrsh25@users.noreply.github.com>
2023-02-08 18:36:23 +0330

    add keypress event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // variables
 
-const buttons = document.querySelectorAll(".button");
+const simpleBtns = document.querySelectorAll(".simple>.button");
 const displayTop = document.querySelector(".displayTop");
 const displayBottom = document.querySelector(".displayBottom");
 const btnPart = document.querySelector("#buttons");
@@ -9,83 +9,85 @@ const btnPart = document.querySelector("#buttons");
 // bad az click shodan har dokme dar safhe, be nesbate shart haei ke vojood dare function calculate() run mishe
 
 btnPart.addEventListener("click", (e) => {
-    if (e.target.classList.contains("button")) {
-        const clicked = e.target;
+  if (e.target.classList.contains("button")) {
+    const clicked = e.target;
 
-        switch (clicked.textContent) {
-            case "C":
-                displayTop.textContent = "";
-                displayBottom.textContent = "0";
-                break;
-            case "CE":
-                displayTop.textContent = displayTop.textContent.slice(0, -1);
-                break;
-            case "x":
-                displayTop.textContent += "*";
-                break;
-            case "sin":
-                displayTop.textContent += "sin(";
-                break;
-            case "cos":
-                displayTop.textContent += "cos(";
-                break;
-            case "tan":
-                displayTop.textContent += "tan(";
-                break;
-            case "cot":
-                displayTop.textContent += "cot(";
-                break;
-            case "log":
-                displayTop.textContent += "log(";
-                break;
-            case "√":
-                displayTop.textContent += "sqrt(";
-                break;
-            case "!":
-                displayTop.textContent += "factorial(";
-                break;
-            case "Rad":
-                // clicked.classList.toggle("on");
-                displayTop.textContent += "radian(";
+    switch (clicked.textContent) {
+      case "C":
+        displayTop.textContent = "";
+        displayBottom.textContent = "0";
+        break;
+      case "CE":
+        displayTop.textContent = displayTop.textContent.slice(0, -1);
+        break;
+      case "x":
+        displayTop.textContent += "*";
+        break;
+      case "sin":
+        displayTop.textContent += "sin(";
+        break;
+      case "cos":
+        displayTop.textContent += "cos(";
+        break;
+      case "tan":
+        displayTop.textContent += "tan(";
+        break;
+      case "cot":
+        displayTop.textContent += "cot(";
+        break;
+      case "log":
+        displayTop.textContent += "log(";
+        break;
+      case "√":
+        displayTop.textContent += "sqrt(";
+        break;
+      case "!":
+        displayTop.textContent += "factorial(";
+        break;
+      case "Rad":
+        // clicked.classList.toggle("on");
+        displayTop.textContent += "radian(";
 
-                break;
-            case "=":
-                // displayTop.textContent = "";
-                const correct = displayTop.textContent
-                    .replaceAll("sin", "Math.sin")
-                    .replaceAll("cos", "Math.cos")
-                    .replaceAll("log", "Math.log2")
-                    .replaceAll("sqrt", "Math.sqrt")
-                    .replaceAll("tan", "Math.tan")
-                    .replaceAll("cot", "Math.cot");
+        break;
+      case "=":
+        // displayTop.textContent = "";
+        const correct = displayTop.textContent
+          .replaceAll("sin", "Math.sin")
+          .replaceAll("cos", "Math.cos")
+          .replaceAll("log", "Math.log2")
+          .replaceAll("sqrt", "Math.sqrt")
+          .replaceAll("tan", "Math.tan")
+          .replaceAll("cot", "Math.cot");
 
-                displayBottom.textContent = evaluation(String(eval(correct)));
+        displayBottom.textContent = evaluation(String(eval(correct)));
 
-                break;
-            default:
-                displayTop.textContent += clicked.textContent;
-                break;
-        }
+        break;
+      default:
+        displayTop.textContent += clicked.textContent;
+        break;
     }
+  }
 });
+// TITLE : event listner baraye har dokme dar keyboard
+window.addEventListener("keypress", match);
 
 // functions
 
 // TITLE : factorial calculation
 function factorial(num) {
-    let result = num;
-    if (num === 0 || num === 1) {
-        return 1;
-    }
-    while (num > 1) {
-        num--;
-        result *= num;
-    }
-    return result;
+  let result = num;
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  while (num > 1) {
+    num--;
+    result *= num;
+  }
+  return result;
 }
 // TITLE : radian calculation
 function radian(deg) {
-    return (deg * Math.PI) / 180;
+  return (deg * Math.PI) / 180;
 }
 
 // TITLE : hesab kardan araqame bad az ashar
@@ -93,26 +95,44 @@ function radian(deg) {
 // agar raqam bade ashar kamtar az 2 bood (0.5), 1 raqam be oon ezafe mishe (0.50)
 
 function evaluation(str) {
-    // if number is decimal
-    if (Number(str) % 1 !== 0) {
-        let strArray = str.split(".");
-        let numbersAfterDecimal = strArray[1];
+  // if number is decimal
+  if (Number(str) % 1 !== 0) {
+    let strArray = str.split(".");
+    let numbersAfterDecimal = strArray[1];
 
-        if (numbersAfterDecimal.length > 5) {
-            numbersAfterDecimal = numbersAfterDecimal.slice(0, 5);
+    if (numbersAfterDecimal.length > 5) {
+      numbersAfterDecimal = numbersAfterDecimal.slice(0, 5);
 
-            str = `${strArray[0]}.${numbersAfterDecimal}`;
+      str = `${strArray[0]}.${numbersAfterDecimal}`;
 
-            return str;
-        } else if (numbersAfterDecimal <= 2) {
-
-            str = Number(str).toFixed(2);
-
-        }
-        return str;
+      return str;
+    } else if (numbersAfterDecimal <= 2) {
+      str = Number(str).toFixed(2);
     }
-    // if number is not decimal
-    else {
-        return str;
+    return str;
+  }
+  // if number is not decimal
+  else {
+    return str;
+  }
+}
+
+// TITLE : Match keys
+// agar kelidi az keyboard feshorde shod button motenazerash
+// dar mashinhesab click shavad
+
+function match(e) {
+  simpleBtns.forEach((item) => {
+    if (e.key == item.textContent) {
+      item.click();
+    } else if (e.key == "*") {
+      if (item.textContent == "x") {
+        item.click();
+      }
+    } else if (e.key == "Enter") {
+      if (item.textContent == "=") {
+        item.click();
+      }
     }
+  });
 }
